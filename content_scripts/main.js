328ffa1b710e098bd4675af603de23eea0deaed4
jmmaniego <jmmaniego@hotmail.com>
2016-09-15 12:39:41 +0800

    refactor - similar to backbone

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,10 +1,23 @@
 WhatsLang.content = {}
 WhatsLang.content.whatslang_container_id = 'whatslang-container';
 
-WhatsLang.content.WhatsLangDialog = function(){
+WhatsLang.content.TextModel = function(text) {
+  var _this = this;
+  _this.text = text;
+  _this.set_text = function(text) {
+    _this.text = text;
+    _this.onchanged.call(this, text);
+  };
+
+  _this.onchanged = function() {};
+}
+
+WhatsLang.content.View = function(model){
   var _this = this;
   var container_id = WhatsLang.content.whatslang_container_id;
   var _translating = false;
+
+  _this.model = model;
 
   _this.templates = {
     container: function() {
@@ -18,32 +31,33 @@ WhatsLang.content.WhatsLangDialog = function(){
     $('body').prepend($container);
   }
 
-  _this.setText = function(text){
+  _this.set_text = function(text){
     return _this.$el.text(text) && this;
   }
 
-  _this.update_text = function(text) {
-    if (!_translating) {
-      _this.setText('Translating...');
-      _translating = true;
-    }
-
-    WhatsLang.debounced_function(function() {
-      chrome.runtime.sendMessage({action: 'translate', params: {word: text}}, function(response) {
-        _this.setText(response.translated_text);
-        _translating = false;
-      });
-    })
+  _this.model.onchanged = function(text) {
+    _this.set_text(text);
   }
 
   _init();
   return this;
 }
 
-var $whatslang_container = new WhatsLang.content.WhatsLangDialog();
+var whastlang_text = new WhatsLang.content.TextModel("");
+var whastlang_view = new WhatsLang.content.View(whastlang_text);
 
 $('body').on('keyup', 'div[contenteditable], textarea, input[type=text]', function() {
   var $this = $(this);
   var word_to_translate = $this.val() || $this.text();
-  $whatslang_container.update_text(word_to_translate);
+  if (!$this.data('translating')) {
+    whastlang_text.set_text("Translating...");
+    $this.data('translating', true);
+  }
+
+  WhatsLang.debounced_function(function() {
+    chrome.runtime.sendMessage({action: 'translate', params: {word: word_to_translate}}, function(response) {
+      whastlang_text.set_text(response.translated_text);
+      $this.data('translating', false);
+    });
+  })
 })
